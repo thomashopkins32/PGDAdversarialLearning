ab08d0ed09cbb06c2f81eeee211e1ad57d4ddfa3
fanga2 <fanga2>
2021-11-03 23:26:59 -0400

    added script for downloading and preprocessing imagenet

---
+++ imagenet.py
@@ -0,0 +1,44 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+# Helper libraries
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Get imagenet labels
+labels_path = tf.keras.utils.get_file('ImageNetLabels.txt','https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
+imagenet_labels = np.array(open(labels_path).read().splitlines())
+
+# Set data_dir to a read-only storage of .tar files
+# Set write_dir to a w/r storage
+data_dir = 'datasets/imagenet/'
+write_dir = 'psando/tf-imagenet-dirs'
+
+# Construct a tf.data.Dataset
+download_config = tfds.download.DownloadConfig(
+                      extract_dir=os.path.join(write_dir, 'extracted'),
+                      manual_dir=data_dir
+                  )
+download_and_prepare_kwargs = {
+    'download_dir': os.path.join(write_dir, 'downloaded'),
+    'download_config': download_config,
+}
+ds = tfds.load('imagenet2012_subset', 
+               data_dir=os.path.join(write_dir, 'data'),         
+               split='train', 
+               shuffle_files=False, 
+               download=True, 
+               as_supervised=True,
+               download_and_prepare_kwargs=download_and_prepare_kwargs)
+
+def resize_with_crop(image, label):
+    i = image
+    i = tf.cast(i, tf.float32)
+    i = tf.image.resize_with_crop_or_pad(i, 224, 224)
+    i = tf.keras.applications.mobilenet_v2.preprocess_input(i)
+    return (i, label)
+
+# Preprocess the images
+ds = ds.map(resize_with_crop)
